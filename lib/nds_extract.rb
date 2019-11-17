8543bd9bad25061a1ad7f69c3b41769039bc24a0
Allen Zhang <azhang9328@gmail.com>
2019-11-17 10:17:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,6 +45,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  puts list_of_directors[0]
 end
 
 
